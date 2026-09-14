58fcc4b30fd4de34913ba264230ea324ec6ed7ae
saifullah <saifullahahad0@gmail.com>
2026-09-14 13:44:05 +0530

    find disapper array number using cyclic sort

--- disapper.java
+++ disapper.java
@@ -1,18 +1,26 @@
-import java.util.*;
+import java.util.ArrayList;
 
-public class ArrayDuplicates {
+public class disapper {
 
-    public static List<Integer> findDuplicates(int[] arr) {
+    public static void main(String[] args) {
 
+        int[] arr = {1, 2, 5, 3, 4, 3, 4};
+
+        System.out.println(dis(arr));
+    }
+
+    public static ArrayList<Integer> dis(int[] arr) {
+
+        int n = arr.length;
         int i = 0;
 
-        while (i < arr.length) {
+       
+        while (i < n) {
 
             int correctIndex = arr[i] - 1;
 
             if (arr[i] != arr[correctIndex]) {
 
-        
                 int temp = arr[i];
                 arr[i] = arr[correctIndex];
                 arr[correctIndex] = temp;
@@ -22,23 +30,16 @@ public class ArrayDuplicates {
             }
         }
 
-        List<Integer> ans = new ArrayList<>();
+        
+        ArrayList<Integer> ans = new ArrayList<>();
 
-    
         for (i = 0; i < arr.length; i++) {
 
             if (arr[i] != i + 1) {
-                ans.add(arr[i]);
+                ans.add(i + 1);
             }
         }
 
         return ans;
-    }
-
-    public static void main(String[] args) {
-
-        int[] arr = {4, 3, 2, 7, 8, 2, 3, 1};
-
-        System.out.println(findDuplicates(arr));
     }
 }
